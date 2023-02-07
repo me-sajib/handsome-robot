445ed3c6d2791573fa482d49250a9b07779c9fa3
me-sajib <me.mrsajib@gmail.com>
2023-02-07 20:33:22 +0600

    change the permission

--- src/Pages/Dashboard/Admin/ManageProduct.js
+++ src/Pages/Dashboard/Admin/ManageProduct.js
@@ -4,47 +4,45 @@ import Spinner from "../../Shared/Spinner";
 import AllProduct from "./AllProduct";
 
 const ManageProduct = () => {
-  const {
-    data: product,
-    isLoading,
-    refetch,
-  } = useQuery("AllProducts", () =>
-    fetch("http://localhost:5000/tools").then((res) =>
-      res.json()
-    )
-  );
+	const {
+		data: product,
+		isLoading,
+		refetch,
+	} = useQuery("AllProducts", () =>
+		fetch("http://localhost:5000/tools").then((res) => res.json())
+	);
 
-  if (isLoading) return <Spinner />;
+	if (isLoading) return <Spinner />;
 
-  return (
-    <div>
-      <h2 className="text-3xl text-orange-500 uppercase py-5">
-        Manage Product
-      </h2>
-      <table className="table w-full my-4">
-        <thead>
-          <tr>
-            <th></th>
-            <th>Tool name</th>
-            <th>Price</th>
-            <th>Available</th>
-            <th>Image</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {product.map((tool, index) => (
-            <AllProduct
-              product={tool}
-              index={index}
-              key={tool._id}
-              refetch={refetch}
-            ></AllProduct>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+	return (
+		<div>
+			<h2 className="text-3xl text-orange-500 uppercase py-5">
+				Manage Robots
+			</h2>
+			<table className="table w-full my-4">
+				<thead>
+					<tr>
+						<th></th>
+						<th>Tool name</th>
+						<th>Price</th>
+						<th>Available</th>
+						<th>Image</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					{product.map((tool, index) => (
+						<AllProduct
+							product={tool}
+							index={index}
+							key={tool._id}
+							refetch={refetch}
+						></AllProduct>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default ManageProduct;
